e3f0e307c1538f7229c97d3e5523033f432358b2
gathdar <33199129+gathdar@users.noreply.github.com>
2018-06-01 19:28:53 -0700

    guess a season

---
+++ do_while_drill/do_while_drill/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// tried to do an enum here but i couldn't get the value of the items
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] seasons = new string[] { "spring", "summer", "fall", "winter" };
+            Console.WriteLine("Can you guess my favorite season? (use 'fall' instead of autumn please)");
+            string input = "";
+            bool isGuessed = input == seasons[1];
+
+            do
+            {
+                input = Console.ReadLine().ToLower();
+                switch (input) { 
+                    case "spring": Console.WriteLine("Nope. Try again.");
+                        break;
+                    case "summer": Console.WriteLine("That's right!");
+                        isGuessed = true;
+                        Console.Read();
+                        break;
+                    case "fall": Console.WriteLine("Nope. Try again.");
+                        break;
+                    case "winter": Console.WriteLine("Nope. Try again.");
+                        break;
+                    default: Console.WriteLine("Do you even spell bro?");
+                        break;
+                }
+            }
+            while (!isGuessed);
+
+        }
+    }
