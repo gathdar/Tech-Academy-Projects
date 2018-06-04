d8e8596dbe0e23ef22880c385b6790ec9e7dc786
gathdar <33199129+gathdar@users.noreply.github.com>
2018-06-03 18:02:23 -0700

    class and method drill

---
+++ CS_course_page44/CS_course_page44/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CS_course_page44
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations doIt = new Operations();
+
+            Console.WriteLine("This program will perform some operations on a number you provide.\nPlease input an integer.");
+            try
+            {
+
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(Operations.Addition(number));
+                Console.WriteLine(Operations.Squared(number));
+                Console.WriteLine(Operations.Subtraction(number));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ CS_course_page44/CS_course_page44/operations.cs
@@ -0,0 +1,22 @@
+﻿namespace CS_course_page44
+{
+    public class Operations
+    {
+        // it doesn't look like the constructor is necessary
+
+        public static int Addition(int number)
+        {
+            return number + 5;
+        }
+
+        public static int Squared(int number)
+        {
+            return number * number;
+        }
+
+        public static int Subtraction(int number)
+        {
+            return number - 100;
+        }
+    }
+}
