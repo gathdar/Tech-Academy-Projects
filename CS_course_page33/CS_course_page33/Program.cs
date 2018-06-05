c1048225231724af5358b85d99f8841ab4378ee7
gathdar <33199129+gathdar@users.noreply.github.com>
2018-06-05 10:02:50 -0700

    added exception handling and fixed variable bug

--- CS_course_page33/CS_course_page33/Program.cs
+++ CS_course_page33/CS_course_page33/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 
-
-
 class Program
 {
     static void Main()
@@ -11,53 +9,67 @@ class Program
         string[] sentence = new string[] { "The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog." };
         Console.WriteLine("I am thinking of a sentence. Select an index (starting at 0), and I will retrieve the corresponding word.");
         string input = Console.ReadLine();
-        int inputNum = Convert.ToInt32(input);
-
-        // notification if the user selects an index that doesn't exist
-        // note: this if statement will not catch letters, symbols or decimals, it will only catch whole numbers outside of the array
-        // i have not accounted for all boneheaded inputs
-        if (inputNum < 0 || inputNum > 8)
+        try
         {
-            Console.WriteLine("Keep within the bounds of the array next time.");
-            Console.ReadLine();
-        }
-        else
+            int inputNum = Convert.ToInt32(input);
+            // notification if the user selects an index that doesn't exist
+            if (inputNum < 0 || inputNum > 8)
+            {
+                Console.WriteLine("Keep within the bounds of the array next time.");
+            }
+            else
+            {
+                Console.WriteLine("{0}\n", sentence[inputNum]);
+            }
+        } 
+        catch (Exception ex)
         {
-            Console.WriteLine("{0}\n", sentence[inputNum]);
+            // this message will display if the user enters something other than a number
+            Console.WriteLine(ex.Message);
         }
 
         // array of integers
         int[] primeNumbers = new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
         Console.WriteLine("I have an array of ten prime numbers. Select an index (starting at 0), and I will retrieve the corresponding number.");
         string input1 = Console.ReadLine();
-        int inputNum1 = Convert.ToInt32(input1);
-
-        // notification if the user selects an index that doesn't exist
-        if (inputNum < 0 || inputNum > 9)
+        try
         {
-            Console.WriteLine("Keep within the bounds of the array next time.");
-            Console.ReadLine();
+            int inputNum1 = Convert.ToInt32(input1);
+            // notification if the user selects an index that doesn't exist
+            if (inputNum1 < 0 || inputNum1 > 9)
+            {
+                Console.WriteLine("Keep within the bounds of the array next time.");
+            }
+            else
+            {
+                Console.WriteLine("{0}\n", primeNumbers[inputNum1]);
+            }
         }
-        else
+        catch (Exception ex)
         {
-            Console.WriteLine("{0}\n", primeNumbers[inputNum1]);
-
+            Console.WriteLine(ex.Message);
         }
 
         // list of strings
         List<string> stringList = new List<string> { "Can", "anybody", "find", "me", "somebody", "to", "love?" };
         Console.WriteLine("I have a list of seven words. Select an index (starting at 0), and I will retrieve the corresponding word.");
         string input2 = Console.ReadLine();
-        int inputNum2 = Convert.ToInt32(input2);
-        if (inputNum < 0 || inputNum > 6)
+        try
         {
-            Console.WriteLine("Keep within the bounds of the list next time.");
-            Console.ReadLine();
+            int inputNum2 = Convert.ToInt32(input2);
+            if (inputNum2 < 0 || inputNum2 > 6)
+            {
+                Console.WriteLine("Keep within the bounds of the list next time.");
+            }
+            else
+            {
+                Console.WriteLine("{0}", stringList[inputNum2]);
+            }
         }
-        else
+        catch (Exception ex)
         {
-            Console.WriteLine("{0}", stringList[inputNum2]);
-            Console.ReadLine();
+            Console.WriteLine(ex.Message);
         }
+        Console.ReadLine();
     }
 }
