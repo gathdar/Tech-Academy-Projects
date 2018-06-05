552fdf00f44a6d931743916ae6f9834a0bb0f42b
gathdar <33199129+gathdar@users.noreply.github.com>
2018-06-05 10:10:18 -0700

    reupload

---
+++ CS_course_Page27/CS_course_Page27/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+    {
+        static void Main()
+        {
+        Random rnd = new Random();
+        int compNum = rnd.Next(1, 10);
+        Console.WriteLine("I am thinking of a number between 1 and 10. \n Try and guess it.");
+        string inputStr;
+        int inputNum = 0;
+        bool isGuessed = (inputNum == compNum);
+        while (!isGuessed)
+        {
+            inputStr = Console.ReadLine();
+            bool goodUser = int.TryParse(inputStr, out inputNum);
+            if (goodUser)
+            {
+                if (inputNum > compNum)
+                {
+                    Console.WriteLine("My number is lower. Try again.");
+                }
+                else if (inputNum < compNum)
+                {
+                    Console.WriteLine("My number is higher. Try again.");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Try again.");
+            }
+
+            
+        }
+        Console.WriteLine("Well done! The number was {0}.", compNum);
+        Console.ReadLine();
+        }
+    }
+
