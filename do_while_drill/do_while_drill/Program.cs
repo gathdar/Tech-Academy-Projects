031d5d1b949ab8a2055ce9c0f8de1f8f632c184d
gathdar <33199129+gathdar@users.noreply.github.com>
2018-06-05 10:30:02 -0700

    added booleans and functionality for autumn

--- do_while_drill/do_while_drill/Program.cs
+++ do_while_drill/do_while_drill/Program.cs
@@ -14,25 +14,48 @@ using System.Threading.Tasks;
             string input = "";
             bool isGuessed = input == seasons[1];
 
-            do
-            {
-                input = Console.ReadLine().ToLower();
-                switch (input) { 
-                    case "spring": Console.WriteLine("Nope. Try again.");
-                        break;
-                    case "summer": Console.WriteLine("That's right!");
-                        isGuessed = true;
-                        Console.Read();
-                        break;
-                    case "fall": Console.WriteLine("Nope. Try again.");
-                        break;
-                    case "winter": Console.WriteLine("Nope. Try again.");
-                        break;
-                    default: Console.WriteLine("Do you even spell bro?");
-                        break;
+        do
+        {
+            input = Console.ReadLine().ToLower();
+                if (input == seasons[0])
+                {
+                    Console.WriteLine("Nope. Try again.");
                 }
+                else if (input == seasons[1])
+                {
+                    Console.WriteLine("That's right!");
+                    isGuessed = true;
+                }
+                else if (input == seasons[2] || input == "autumn")
+                {
+                    Console.WriteLine("Nope. Try again.");
+                }
+                else if (input == seasons[3])
+                {
+                    Console.WriteLine("Nope. Try again.");
+                }
+                else
+                {
+                    Console.WriteLine("Please type an actual season.");
+                }
+
+                //switch (input) { 
+                //    case "spring": Console.WriteLine("Nope. Try again.");
+                //        break;
+                //    case "summer": Console.WriteLine("That's right!");
+                //        isGuessed = true;
+                //        Console.Read();
+                //        break;
+                //    case "fall": Console.WriteLine("Nope. Try again.");
+                //        break;
+                //    case "winter": Console.WriteLine("Nope. Try again.");
+                //        break;
+                //    default: Console.WriteLine("Do you even spell bro?");
+                //        break;
+                //}
+
             }
             while (!isGuessed);
-
+        Console.ReadLine();
         }
     }
