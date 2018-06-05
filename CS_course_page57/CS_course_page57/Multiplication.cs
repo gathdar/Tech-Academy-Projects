0edb3a028336325cf47124dff621dd21059a2976
gathdar <33199129+gathdar@users.noreply.github.com>
2018-06-05 14:36:11 -0700

    class and method drill

---
+++ CS_course_page57/CS_course_page57/Multiplication.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_course_page57
+{
+    class Multiplication
+    {
+        public static int Multiplier(int operand)
+        {
+            int newNumber = operand * 10;
+            return newNumber;
+        }
+
+        public static decimal NewMultiplier(decimal myNumber)
+        {
+            decimal newNumber = myNumber * myNumber;
+            return newNumber;
+        }
+
+        public static int Converter(string myString)
+        {
+            bool works = int.TryParse(myString, out int convertedString);
+            int newConvertedString = convertedString * 3;
+            return newConvertedString;
+        }
+    }
+}
---
+++ CS_course_page57/CS_course_page57/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CS_course_page57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Multiplication operation = new Multiplication();
+            int operand = 3;
+            int answer = Multiplication.Multiplier(operand);
+            Console.WriteLine("The answer is: {0}", answer);
+            Console.ReadLine();
+
+            decimal myNumber = 345.345678m;
+            decimal newAnswer = Multiplication.NewMultiplier(myNumber);
+            Console.WriteLine("The new answer is: {0}", newAnswer);
+            Console.ReadLine();
+
+            string reallyANumber = "345";
+            int result = Multiplication.Converter(reallyANumber);
+            Console.WriteLine("The newest answer is: {0}", result);
+            Console.ReadLine();
+        }
+    }
+}
