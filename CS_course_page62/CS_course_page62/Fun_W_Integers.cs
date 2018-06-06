3a3f558e88a45a2d9818f4558ab91f6b7114d8e1
gathdar <33199129+gathdar@users.noreply.github.com>
2018-06-06 12:48:45 -0700

    class with void method

---
+++ CS_course_page62/CS_course_page62/Fun_W_Integers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_course_page62
+{
+    // step 1, create the class with a void method
+    class Fun_W_Integers
+    {
+        public void Operation (int num1, int num2)
+        {
+            double num3 = Math.Exp(num1) / Math.Log(num1);
+
+            Console.WriteLine("The result of my operation on your first number is: {0}", num3);
+            Console.WriteLine("The second number you typed is {0}", num2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CS_course_page62/CS_course_page62/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_course_page62
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // step 2, instantiate the class
+            Fun_W_Integers hello = new Fun_W_Integers();
+            bool success1 = false;
+            bool success2 = false;
+            int num1 = 0;
+            int num2 = 0;
+            // here i wanted to keep giving options to input correctly
+            while (!success1 || !success2)
+            {
+                Console.WriteLine("Please enter two integers.");
+                string input1 = Console.ReadLine();
+                string input2 = Console.ReadLine();
+                success1 = int.TryParse(input1, out num1);
+                success2 = int.TryParse(input2, out num2);
+            }
+
+            // step 3 call the method of the class
+            hello.Operation(num1, num2);
+            // step 4 call the method but specify the parameters by name
+            hello.Operation(num1: num1, num2: num2);
+ 
+
+           
+        }
+    }
+}
