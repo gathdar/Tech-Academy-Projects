6cef9d4f5b33fcaebb91941a76e5c01f126f7108
gathdar <33199129+gathdar@users.noreply.github.com>
2018-06-08 14:59:36 -0700

    fixed problem with generic list property
    i had to separately instantiate the list property so it could accept values
    another way to do this is in the constructor

--- CS_course_page79/CS_course_page79/Program.cs
+++ CS_course_page79/CS_course_page79/Program.cs
@@ -13,38 +13,41 @@ namespace CS_course_page79
             Employee<string> employee = new Employee<string>()
             {
                 FirstName = "George",
-                LastName = "Marshall"
-                Things = {"hello", "my", "name", "is", "george"}
+                LastName = "Marshall",
+                // Things = {"hello", "my", "name", "is", "george"}
             };
 
+            if (employee.Things == null) // this is irrelevant, see line 39, but it is important to know that it remains null if not instantiated separately
+            {
+                employee.Things = new List<string>();
+            }
 
+            employee.Things.Add("hello");
+            employee.Things.Add("my");
+            employee.Things.Add("name");
+            employee.Things.Add("is");
+            employee.Things.Add("george");
+            // employee.Things = new List<string>(); i tried this earlier but placing it here was too late
 
-            //employee.Things.Add("hello");
-            //employee.Things.Add("my");
-            //employee.Things.Add("name");
-            //employee.Things.Add("is");
-            //employee.Things.Add("george");
-            // employee.Things = new List<string>();
+            Employee<int> employee1 = new Employee<int>()
+            {
+                FirstName = "Mike",
+                LastName = "Jones",
+                //Things = { 1, 2, 3, 4, 5 }
+                // this cannot be instantiated here because the list/property needs to be separately instantiated
+            };
+            employee1.Things = new List<int>() { 1, 2, 3, 4, 5 };
 
-            //Employee<int> employee1 = new Employee<int>();
-            //{
-            //    //FirstName = "Mike",
-            //    //LastName = "Jones",
-            //    Things = { 1, 2, 3, 4, 5 }
-            //};
+            Console.WriteLine(employee.FirstName);
+            Console.WriteLine(employee1.FirstName);
 
-            //Console.WriteLine(employee.FirstName);
-            //Console.WriteLine(employee1.FirstName);
+            foreach (int i in employee1.Things)
+                Console.WriteLine(employee1.Things[i-1]);
 
-            //foreach (string i in employee)
-            //    Console.WriteLine(employee[i]);
-
-            //for (int i = 0; i < employee.Count; i++)
-            //{
-            //    Console.WriteLine(employee[i]);
-            //}
-
-            Console.WriteLine(string.Join(" ", employee));
+            for (int i = 0; i < employee.Things.Count; i++)
+            {
+                Console.WriteLine(employee.Things[i]);
+            }
 
             Console.ReadLine();
         }
