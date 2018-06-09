9c293d35f37fbf68332716981711907946710ea0
gathdar <33199129+gathdar@users.noreply.github.com>
2018-06-08 20:25:45 -0700

    cast input to type enum

---
+++ CS_course_page82/CS_course_page82/DaysOfWeek.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_course_page82
+{
+    class DaysOfWeek
+    {
+
+        public enum Days
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
---
+++ CS_course_page82/CS_course_page82/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_course_page82
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DaysOfWeek daysofWeek = new DaysOfWeek();
+
+            bool isCorrect = false;
+            
+            Console.WriteLine("Greetings. Please enter the current day of the week.");
+            while (!isCorrect)
+            {
+                string input = Console.ReadLine();
+                try
+                {
+                    DaysOfWeek.Days newDay = (DaysOfWeek.Days)Enum.Parse(typeof(DaysOfWeek.Days), input);
+                    Console.WriteLine("Today is {0}", newDay);
+                    isCorrect = true;
+                    Console.ReadLine();
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter an actual day of the week.");
+                }
+            }
+            
+
+        }
+    }
+}
