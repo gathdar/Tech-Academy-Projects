0d244791651eb1827dca1ab3460e361e56e9aa19
Justin Moore <33199129+sirjust@users.noreply.github.com>
2018-06-11 13:06:35 -0700

    struct practice

---
+++ CS_course_page85/CS_course_page85/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_course_page85
+{
+    struct Number
+    {
+        public decimal Amount;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number() { Amount = 35.55m };
+
+            Console.WriteLine("Your amount is: {0}", number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
