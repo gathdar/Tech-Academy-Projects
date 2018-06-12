7c4f37e3696f9c3e5ca612d0f186c9a2c7f94946
Justin Moore <33199129+sirjust@users.noreply.github.com>
2018-06-12 15:03:32 -0700

    file I/O drill

---
+++ CS_course_page102/CS_course_page102/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CS_course_page102
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool finished = false;
+            while (!finished)
+            {
+                Console.WriteLine("Greetings. Please enter a number.");
+                bool isNumber = double.TryParse(Console.ReadLine(), out double input);
+                if (isNumber)
+                {
+                    using (StreamWriter file = new StreamWriter(@"C:\Users\SirJUST\Logs\numberlog.txt", true))
+                    {
+                        file.WriteLine(input);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("I didn't recognize your format. Please try again.");
+                    continue;
+                }
+                Console.WriteLine("The last entry was: {0}", input);
+                string totalFile = File.ReadAllText("C:\\Users\\SirJUST\\Logs\\numberlog.txt");
+                Console.WriteLine("Here is the file in its entirety:\n{0}", totalFile);
+                finished = true;
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
