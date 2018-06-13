e5b17643bec986b0e7c792fbd192f7d09c5f4c00
Justin Moore <33199129+sirjust@users.noreply.github.com>
2018-06-13 15:09:10 -0700

    const, var, ccc drill

---
+++ CS_course_page111/CS_course_page111/Bicycle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_course_page111
+{
+    class Bicycle
+    {
+        public Bicycle(string name) : this(name, 2, "unknown", 5, "unknown")
+        {
+
+        }
+        public Bicycle(string name, int wheels, string size, int speeds, string color)
+        {
+            Name = name;
+            Wheels = wheels;
+            Size = size;
+            Speeds = speeds;
+            Color = color;
+        }
+
+        public string Name { get; set; }
+        public int Wheels { get; set; }
+        public string Size { get; set; }
+        public int Speeds { get; set; }
+        public string Color { get; set; }
+
+    }
+}
---
+++ CS_course_page111/CS_course_page111/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_course_page111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string cannotChange = "A constant cannot change, be set to a value that could change,\nor use an implicitly typed variable.";
+            Console.WriteLine(cannotChange);
+            var bicycle = new Bicycle("Speedy");
+            Bicycle bicycle1 = new Bicycle("Slowpoke", 3, "small", 2, "orange");
+            Console.WriteLine("{0} has fewer wheels ({1}) than {2} which has {3}.\nThe reason is because of my constructor call chain.", bicycle.Name, bicycle.Wheels, bicycle1.Name, bicycle1.Wheels);
+            Console.ReadLine();
+        }
+    }
+}
